334e4a239c2a268da5fa857867b719f55ce98247
Luiz Rioja <lulyrioja@gmail.com>
2021-04-18 17:56:01 -0300

    :alien: Solving the Diagonal Difference challenge with js

--- Diagonal Difference/diagonalDifference.js
+++ Diagonal Difference/diagonalDifference.js
@@ -7,3 +7,41 @@
 // The left-to-right diagonal = 1 + 5 + 9 = 15.
 // The right to left diagonal = 3 + 5 + 9 = 17.
 // Their absolute difference is | 15 - 17| = 2.
+
+var array = [
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12],
+];
+
+function getDiagonalDifference(array) {
+  let leftToRight = getSumDiagonalLeftRight(array);
+  let rightToLeft = getSumDiagonalRightLeft(array);
+  let difference = leftToRight - rightToLeft;
+
+  return Math.abs(difference);
+}
+
+function getSumDiagonalLeftRight(array) {
+  let sumDiagonal = 0;
+
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index][index];
+    sumDiagonal += element;
+  }
+
+  return sumDiagonal;
+}
+
+function getSumDiagonalRightLeft(array) {
+  let sumDiagonal = 0;
+
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index][array.length - 1 - index];
+    sumDiagonal += element;
+  }
+
+  return sumDiagonal;
+}
+
+console.log(getDiagonalDifference(array));
