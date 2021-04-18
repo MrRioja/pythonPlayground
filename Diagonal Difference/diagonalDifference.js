bd378d1cb86d04a3135fc34b6770f4418d7b72e4
Luiz Rioja <lulyrioja@gmail.com>
2021-04-18 14:36:49 -0300

    :alien: Adding the two new challenges

---
+++ Diagonal Difference/diagonalDifference.js
@@ -0,0 +1,9 @@
+// Problem: https://www.hackerrank.com/challenges/diagonal-difference/problem
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+// For example, the square matrix arr is shown below:
+// 1 2 3
+// 4 5 6
+// 9 8 9
+// The left-to-right diagonal = 1 + 5 + 9 = 15.
+// The right to left diagonal = 3 + 5 + 9 = 17.
+// Their absolute difference is | 15 - 17| = 2.
