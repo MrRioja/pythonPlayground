ce8b62da6975f4d991b61cf8fc1df92845162611
Luiz Rioja <lulyrioja@gmail.com>
2021-05-01 23:50:20 -0300

    :alien: Solving the staircase in js

---
+++ Staircase/staircase.js
@@ -0,0 +1,30 @@
+// Problem: https://www.hackerrank.com/challenges/staircase/problem
+// Sua base e altura são iguais a N. Ele é desenhado usando #símbolos e espaços. A última linha não é precedida de espaços.
+// Escreva um programa que imprima uma escada de tamanho N.
+// Ex: N = 4:
+//    #
+//   ##
+//  ###
+// ####
+
+function staircase(n) {
+  let staircase = "";
+
+  for (let index = 0; index < n; index++) {
+    if (index > 0) {
+      staircase += "\n";
+    }
+
+    for (let aux = 0; aux < n; aux++) {
+      if (aux + 1 >= n - index) {
+        staircase += "#";
+      } else {
+        staircase += " ";
+      }
+    }
+  }
+
+  return staircase;
+}
+
+console.log(staircase(3));
---
+++ Staircase/staircase.py
@@ -0,0 +1,10 @@
+# Problem: https://www.hackerrank.com/challenges/staircase/problem
+# Sua base e altura são iguais a N. Ele é desenhado usando #símbolos e espaços. A última linha não é precedida de espaços.
+# Escreva um programa que imprima uma escada de tamanho N.
+# Ex: N = 4:
+#    #
+#   ##
+#  ###
+# ####
+
+# def staircase(n):
