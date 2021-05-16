961556745195c5efe8f50095f5c73b8ddb1d0a94
mrrioja <lulyrioja@gmail.com>
2021-05-15 23:48:05 -0300

    :alien: Adding new challenges

---
+++ Breaking the Records/breakingTheRecords.js
@@ -0,0 +1,7 @@
+// Problem: https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+// Maria plays college basketball and wants to go pro. Each season she maintains a record of her play.
+// She tabulates the number of times she breaks her season record for most points and least points in a game.
+// Points scored in the first game establish her record for the season, and she begins counting from there.
+// Example:
+// scores = [12,24,10,24]
+// Scores are in the same order as the games played. She tabulates her results as follows:
