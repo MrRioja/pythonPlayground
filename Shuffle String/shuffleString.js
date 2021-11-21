0a35790df7244f3d171ed5f93ed71dabb4e9fff1
Luiz Rioja <lulyrioja@gmail.com>
2021-11-20 23:46:12 -0300

    :alien: Adding the Shuffle String problem

---
+++ Shuffle String/shuffleString.js
@@ -0,0 +1,4 @@
+// Problem: https://leetcode.com/problems/shuffle-string/
+// Given a string s and an integer array indices of the same length.
+// The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+// Return the shuffled string.
