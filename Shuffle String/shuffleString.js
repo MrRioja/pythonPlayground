86073b24ae9869cfa9fc5a96dd7dbcba55f2e79c
Luiz Rioja <lulyrioja@gmail.com>
2021-11-21 19:43:43 -0300

    :alien: Solving the Shuffle String Challenge

--- Shuffle String/shuffleString.js
+++ Shuffle String/shuffleString.js
@@ -2,3 +2,25 @@
 // Given a string s and an integer array indices of the same length.
 // The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
 // Return the shuffled string.
+// Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+// Output: "leetcode"
+
+const s = "codeleet";
+const indices = [4, 5, 6, 7, 0, 2, 1, 3];
+
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+function restoreString(s, indices) {
+  let unscrambledWord = "";
+
+  for (let index = 0; index < s.length; index++) {
+    unscrambledWord += s[indices.indexOf(index)];
+  }
+
+  return unscrambledWord;
+}
+
+console.log(restoreString(s, indices));
--- Shuffle String/shuffleString.py
+++ Shuffle String/shuffleString.py
@@ -2,3 +2,26 @@
 # Given a string s and an integer array indices of the same length.
 # The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
 # Return the shuffled string.
+# Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+# Output: "leetcode"
+
+
+from typing import List
+
+s = "codeleet"
+indices = [4, 5, 6, 7, 0, 2, 1, 3]
+
+# s = "aaiougrt"
+# indices = [4, 0, 2, 6, 7, 3, 1, 5]
+
+
+def restoreString(s: str, indices: List[int]) -> str:
+    unscrambledWord = ''
+
+    for index in range(len(s)):
+        unscrambledWord += s[indices.index(index)]
+
+    return unscrambledWord
+
+
+print(restoreString(s, indices))
