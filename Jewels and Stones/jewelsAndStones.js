c51a48acadeca44cee7fb6d13b5d8f3dcc5e2a4c
Luiz Rioja <lulyrioja@gmail.com>
2021-11-21 20:06:41 -0300

    :alien: Solving the Jewels and Stones Challenge

--- Jewels and Stones/jewelsAndStones.js
+++ Jewels and Stones/jewelsAndStones.js
@@ -2,3 +2,32 @@
 // You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have.
 // Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
 // Letters are case sensitive, so "a" is considered a different type of stone from "A".
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+
+const jewels = "aA";
+const stones = "aAAbbbb";
+
+// const jewels = "z";
+// const stones = "ZZ";
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+function numJewelsInStones(jewels, stones) {
+  numJewels = 0;
+
+  for (let i = 0; i < stones.length; i++) {
+    for (let j = 0; j < jewels.length; j++) {
+      if (stones[i] == jewels[j]) {
+        numJewels += 1;
+      }
+    }
+  }
+
+  return numJewels;
+}
+
+console.log(numJewelsInStones(jewels, stones));
--- Jewels and Stones/jewelsAndStones.py
+++ Jewels and Stones/jewelsAndStones.py
@@ -2,3 +2,26 @@
 # You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have.
 # Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
 # Letters are case sensitive, so "a" is considered a different type of stone from "A".
+# Input: jewels = "aA", stones = "aAAbbbb"
+# Output: 3
+
+jewels = "aA"
+stones = "aAAbbbb"
+
+
+# jewels = "z"
+# stones = "ZZ"
+
+
+def numJewelsInStones(jewels: str, stones: str) -> int:
+    numJewels = 0
+
+    for stone in stones:
+        for jewel in jewels:
+            if stone == jewel:
+                numJewels += 1
+
+    return numJewels
+
+
+print(numJewelsInStones(jewels, stones))
