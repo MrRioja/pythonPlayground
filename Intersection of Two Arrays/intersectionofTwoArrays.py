986c3d00e880dd836a6b5a473bcdd77f7df9aae2
Luiz Rioja <lulyrioja@gmail.com>
2021-11-20 23:47:13 -0300

    :alien: Adding the Intersection of Two Arrays problem

---
+++ Intersection of Two Arrays/intersectionofTwoArrays.py
@@ -0,0 +1,3 @@
+# Problem: https://leetcode.com/problems/intersection-of-two-arrays/
+# Given two integer arrays nums1 and nums2, return an array of their intersection.
+# Each element in the result must be unique and you may return the result in any order.
