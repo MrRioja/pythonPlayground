ba00b912c1c30c4206d778784f7f3d038198d51a
Luiz Rioja <lulyrioja@gmail.com>
2021-11-21 20:25:44 -0300

    :alien: Solving the Intersection of Two Arrays challenge

--- Intersection of Two Arrays/intersectionofTwoArrays.js
+++ Intersection of Two Arrays/intersectionofTwoArrays.js
@@ -1,3 +1,32 @@
 // Problem: https://leetcode.com/problems/intersection-of-two-arrays/
 // Given two integer arrays nums1 and nums2, return an array of their intersection.
 // Each element in the result must be unique and you may return the result in any order.
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2]
+
+const nums1 = [1, 2, 2, 1];
+const nums2 = [2, 2];
+
+// const nums1 = [4, 9, 5];
+// const nums2 = [9, 4, 9, 8, 4];
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+function intersection(nums1, nums2) {
+  let intersectionNums = [];
+
+  nums1.forEach((num1) => {
+    nums2.forEach((num2) => {
+      if (num1 == num2 && !intersectionNums.includes(num1)) {
+        intersectionNums.push(num1);
+      }
+    });
+  });
+
+  return intersectionNums;
+}
+
+console.log(intersection(nums1, nums2));
--- Intersection of Two Arrays/intersectionofTwoArrays.py
+++ Intersection of Two Arrays/intersectionofTwoArrays.py
@@ -1,3 +1,28 @@
 # Problem: https://leetcode.com/problems/intersection-of-two-arrays/
 # Given two integer arrays nums1 and nums2, return an array of their intersection.
 # Each element in the result must be unique and you may return the result in any order.
+# Input: nums1 = [1,2,2,1], nums2 = [2,2]
+# Output: [2]
+
+from typing import List
+
+
+nums1 = [1, 2, 2, 1]
+nums2 = [2, 2]
+
+# nums1 = [4, 9, 5]
+# nums2 = [9, 4, 9, 8, 4]
+
+
+def intersection(nums1: List[int], nums2: List[int]) -> List[int]:
+    intersectionNums = []
+
+    for num1 in nums1:
+        for num2 in nums2:
+            if num1 == num2 and num1 not in intersectionNums:
+                intersectionNums.append(num1)
+
+    return intersectionNums
+
+
+print(intersection(nums1, nums2))
