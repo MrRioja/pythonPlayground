1a185e19211299bab43cd17b28688943dc994666
Luiz Rioja <lulyrioja@gmail.com>
2022-05-19 21:09:56 -0300

    refac: standardize folder names

--- List Comprehensions/listComprehensions.py
+++
@@ -1,7 +0,0 @@
-# Problem: https://www.hackerrank.com/challenges/list-comprehensions/problem
-
-if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
---
+++ ListComprehensions/listComprehensions.py
@@ -0,0 +1,12 @@
+# Problem: https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+# if __name__ == '__main__':
+
+def listComprehensions():
+    x = int(input("Digite o valor de x: "))
+    y = int(input("Digite o valor de y: "))
+    z = int(input("Digite o valor de z: "))
+    n = int(input("Digite o valor de n: "))
+
+
+listComprehensions()
