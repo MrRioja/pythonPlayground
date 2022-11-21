70e832fcde135161188710ad075f698f3324fe95
Luiz Rioja <lulyrioja@gmail.com>
2022-11-21 19:38:03 -0300

    feat(PlusMinus): solve challenge with Python

---
+++ PlusMinus/plusMinus.py
@@ -0,0 +1,26 @@
+# Problem: https://www.hackerrank.com/challenges/three-month-preparation-kit-plus-minus/problem?h_r=internal-search
+# Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero.
+# Print the decimal value of each fraction on a new line with 6 places after the decimal.
+# Ex: arr = [1, 1, 0, -1, -1]
+# There are n = 5 elements, two positive, two negative and one zero. Their ratios are 2/5 = 0.400000, 2/5 = 0.400000 and 1/5 = 0.200000.
+# Results are printed as:
+# 0.400000
+# 0.400000
+# 0.200000
+
+
+def plusMinus(arr):
+    positive = [num for num in arr if num > 0]
+    negative = [num for num in arr if num < 0]
+    zero = [num for num in arr if num == 0]
+
+    print(
+        f'{(len(positive)/len(arr)):.6f}',
+        f'\n{len(negative)/len(arr):.6f}',
+        f'\n{len(zero)/len(arr):.6f}'
+    )
+
+
+arr = [1, 1, 0, -1, -1]
+
+plusMinus(arr)
