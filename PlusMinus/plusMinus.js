73873f5ccc659253ac73ae1ae1d7ba19dac85cf3
Luiz Rioja <lulyrioja@gmail.com>
2022-11-21 19:38:10 -0300

    feat(PlusMinus): solve challenge with Javascript

---
+++ PlusMinus/plusMinus.js
@@ -0,0 +1,25 @@
+// Problem: https://www.hackerrank.com/challenges/three-month-preparation-kit-plus-minus/problem?h_r=internal-search
+// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero.
+// Print the decimal value of each fraction on a new line with 6 places after the decimal.
+// Ex: arr = [1, 1, 0, -1, -1]
+// There are n = 5 elements, two positive, two negative and one zero. Their ratios are 2/5 = 0.400000, 2/5 = 0.400000 and 1/5 = 0.200000.
+// Results are printed as:
+// 0.400000
+// 0.400000
+// 0.200000
+
+function plusMinus(arr) {
+  const positive = arr.filter((num) => num > 0);
+  const negative = arr.filter((num) => num < 0);
+  const zero = arr.filter((num) => num === 0);
+
+  console.log(
+    `${(positive.length / arr.length).toFixed(6)}`,
+    `\n${(negative.length / arr.length).toFixed(6)}`,
+    `\n${(zero.length / arr.length).toFixed(6)}`
+  );
+}
+
+const arr = [1, 1, 0, -1, -1];
+
+plusMinus(arr);
