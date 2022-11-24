bc8d676301ce63bb1779beb692f71f51f4d68441
Luiz Rioja <lulyrioja@gmail.com>
2022-11-24 18:59:52 -0300

    feat(LonelyInteger): solve challenge with Python

---
+++ LonelyInteger/lonelyInteger.py
@@ -0,0 +1,16 @@
+# Problem: https://www.hackerrank.com/challenges/three-month-preparation-kit-lonely-integer/problem?h_r=internal-search
+# Given an array of integers, where all elements but one occur twice, find the unique element.
+# Ex: a = [1, 2, 3, 4, 3, 2, 1]. The unique element is 4.
+
+from collections import Counter
+
+
+def lonely_integer(a):
+    int_count = Counter(a)
+
+    return [x for (x, y) in int_count.items() if y == 1][0]
+
+
+a = [1, 2, 3, 4, 3, 2, 1]
+
+print(lonely_integer(a))
