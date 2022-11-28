bf928d8782ac738bc3bac75d5ff8219c6224b3fa
Luiz Rioja <lulyrioja@gmail.com>
2022-11-28 18:15:14 -0300

    feat(BreakingTheRecords): solve challenge with Javascript

--- BreakingTheRecords/breakingTheRecords.js
+++ BreakingTheRecords/breakingTheRecords.js
@@ -5,5 +5,34 @@
 // Example:
 // scores = [12,24,10,24]
 // Scores are in the same order as the games played. She tabulates her results as follows:
+//                                  Count
+// Game  Score  Minimum  Maximum   Min Max
+// 0      12     12       12       0   0
+// 1      24     12       24       0   1
+// 2      10     10       24       1   1
+// 3      24     10       24       1   1
+// Given the scores for a season, determine the number of times Maria breaks her records for most and least points
+// scored during the season.
 
-function breakingRecords(scores) {}
+function breakingRecords(scores) {
+  let min_score = [0, scores[0]];
+  let max_score = [0, scores[0]];
+
+  for (const score of scores) {
+    if (score > max_score[1]) {
+      max_score[0] += 1;
+      max_score[1] = score;
+    }
+    if (score < min_score[1]) {
+      min_score[0] += 1;
+      min_score[1] = score;
+    }
+  }
+
+  console.log(max_score[0], min_score[0]);
+}
+
+// const scores = [12, 24, 10, 24];
+const scores = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42];
+
+breakingRecords(scores);
