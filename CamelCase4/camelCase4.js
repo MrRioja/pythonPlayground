8eafa880b824d62a453eb0e762a8591581be349c
Luiz Rioja <lulyrioja@gmail.com>
2023-01-08 23:00:35 -0300

    chore: add challenge camelCase4

---
+++ CamelCase4/camelCase4.js
@@ -0,0 +1,5 @@
+// Problem: https://www.hackerrank.com/challenges/three-month-preparation-kit-camel-case/problem
+// Camel Case is a naming style common in many programming languages. In Java, method and variable names typically
+// start with a lowercase letter, with all subsequent words starting with a capital letter (example: startThread).
+// Names of classes follow the same pattern, except that they start with a capital letter (example: BlueCar).
+// Your task is to write a program that creates or splits Camel Case variable, method, and class names.
