dc2bbe0c102d05e74791457bed55cbb694e8f7ee
Luiz Rioja <lulyrioja@gmail.com>
2023-01-08 23:06:12 -0300

    chore: add divisibleSumPairs challenge

---
+++ DivisibleSumPairs/divisibleSumPairs.js
@@ -0,0 +1,9 @@
+// Problem: https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+// Given an array of integers and a positive integer k, determine the number of(i, j) pairs where
+// i < j and arr[i] + arr[j] is divisible by k.
+// Example: arr = [1, 2, 3, 4, 5, 6], k = 5
+// Three pairs meet the criteria: [1, 4], [2, 3] and [4, 6].
+
+function divisibleSumPairs(n, k, ar) {
+  return;
+}
