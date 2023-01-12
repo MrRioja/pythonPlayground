7a8f77f9fcf86f6f5711ac265cb211985fd41f9a
Luiz Rioja <lulyrioja@gmail.com>
2023-01-12 15:23:18 -0300

    feat(CamelCase4): solve challenge with Python

--- CamelCase4/camelCase4.py
+++ CamelCase4/camelCase4.py
@@ -3,3 +3,38 @@
 # start with a lowercase letter, with all subsequent words starting with a capital letter (example: startThread).
 # Names of classes follow the same pattern, except that they start with a capital letter (example: BlueCar).
 # Your task is to write a program that creates or splits Camel Case variable, method, and class names.
+# Each line of the input file will begin with an operation (S or C) followed by a semi-colon followed by M, C, or V
+# followed by a semi-colon followed by the words you'll need to operate on.
+# The operation will either be S (split) or C (combine)
+# M indicates method, C indicates class, and V indicates variable
+# In the case of a split operation, the words will be a camel case method, class or variable name that you need to split
+#  into a space-delimited list of words starting with a lowercase letter.
+# In the case of a combine operation, the words will be a space-delimited list of words starting with lowercase letters that
+# you need to combine into the appropriate camel case String. Methods should end with an empty set of parentheses to
+# differentiate them from variable names.
+
+import re
+
+word = input('Type something: ')
+
+[operation, typeValue, value] = word.split(';')
+
+if operation == 'S':
+    allMatch = re.findall('[A-Z]', value)
+
+    for match in allMatch:
+        value = value.replace(match, ' ' + match.lower())
+
+    print(value.replace('()', '').strip())
+else:
+    allMatch = re.findall('\s\w', value)
+
+    for match in allMatch:
+        value = value.replace(match, match.strip().upper())
+
+    if typeValue == 'V':
+        print(value)
+    elif typeValue == 'M':
+        print(value + '()')
+    else:
+        print(value.replace(value[0], value[0].upper(), 1))
