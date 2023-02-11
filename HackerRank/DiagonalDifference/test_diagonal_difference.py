d178bd435a0a125acee992bd5a82430966adcc15
Luiz Rioja <lulyrioja@gmail.com>
2023-02-11 20:11:45 -0300

    test(diagonalDifference): add unit tests

---
+++ HackerRank/DiagonalDifference/test_diagonal_difference.py
@@ -0,0 +1,16 @@
+import pytest
+from diagonal_difference import get_diagonal_difference
+
+
+@pytest.mark.parametrize("array,expected", [
+    ([[11, 2, 4], [4, 5, 6], [10, 8, -12]], 15)
+])
+def test_get_diagonal_difference(array, expected):
+    assert get_diagonal_difference(array) == expected
+
+
+@pytest.mark.parametrize("array,expected", [
+    ([[11, 2, 4], [4, 5, 6], [10, 8, -12]], 0)
+])
+def test_get_diagonal_difference_error(array, expected):
+    assert get_diagonal_difference(array) != expected
