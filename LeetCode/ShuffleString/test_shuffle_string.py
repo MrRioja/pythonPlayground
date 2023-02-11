a79c281682528e13d5db2e5df34598a28ff44889
Luiz Rioja <lulyrioja@gmail.com>
2023-02-11 18:53:47 -0300

    test: change error test name

--- LeetCode/ShuffleString/test_shuffle_string.py
+++ LeetCode/ShuffleString/test_shuffle_string.py
@@ -14,5 +14,5 @@ def test_restore_string(string, indices, expected):
     ("codeleet", [4, 5, 6, 7, 0, 2, 1, 3], "incorrect_value"),
     ("aaiougrt", [4, 0, 2, 6, 7, 3, 1, 5], "incorrect_value"),
 ])
-def test_restore_string(string, indices, expected):
+def test_restore_string_error(string, indices, expected):
     assert restore_string(string, indices) != expected
