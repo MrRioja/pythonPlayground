6e076d59dd44cccbf5aaa039e64e49e331ad8669
Luiz Rioja <lulyrioja@gmail.com>
2023-02-11 20:57:49 -0300

    test(simpleArraySum): add unit tests

--- HackerRank/SimpleArraySum/simple_array_sum.py
+++ HackerRank/SimpleArraySum/simple_array_sum.py
@@ -2,8 +2,6 @@
 # Given an array of integers, find the sum of its elements.
 # For example, if the array ar=[1,2,3], so return 6.
 
-vector = [338, 65, 713, 595, 428, 610, 728, 573, 871, 868]
-
 
 def sum_array_elements(vector):
     total = 0
@@ -12,6 +10,3 @@ def sum_array_elements(vector):
         total += el
 
     return total
-
-
-sum_array_elements(vector)
