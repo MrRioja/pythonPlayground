dd2ca4b5573ca23d82b953c7c8366b3d9a9fc9ef
Luiz Rioja <lulyrioja@gmail.com>
2023-02-11 18:12:35 -0300

    test(shuffleString): add unit tests

---
+++ LeetCode/ShuffleString/test_shuffle_string.py
@@ -0,0 +1,18 @@
+import pytest
+from shuffleString import restore_string
+
+
+@pytest.mark.parametrize("string,indices,expected", [
+    ("codeleet", [4, 5, 6, 7, 0, 2, 1, 3], "leetcode"),
+    ("aaiougrt", [4, 0, 2, 6, 7, 3, 1, 5], "arigatou"),
+])
+def test_shuffle_string(string, indices, expected):
+    assert restore_string(string, indices) == expected
+
+
+@pytest.mark.parametrize("string,indices,expected", [
+    ("codeleet", [4, 5, 6, 7, 0, 2, 1, 3], "incorrect_value"),
+    ("aaiougrt", [4, 0, 2, 6, 7, 3, 1, 5], "incorrect_value"),
+])
+def test_shuffle_string_error(string, indices, expected):
+    assert restore_string(string, indices) != expected
