4269edc4642879ab4cf1742c6d4b28dd0c06d5d9
Luiz Rioja <lulyrioja@gmail.com>
2023-02-11 18:55:30 -0300

    test: fix error test

--- LeetCode/JewelsAndStones/test_jewels_and_stones.py
+++ LeetCode/JewelsAndStones/test_jewels_and_stones.py
@@ -11,8 +11,8 @@ def test_num_jewels_in_stones(jewels, stones, expected):
 
 
 @pytest.mark.parametrize("jewels,stones,expected", [
-    ("z", "ZZ", 0),
-    ("aA", "aAAbbbb", 3)
+    ("z", "ZZ", 1000),
+    ("aA", "aAAbbbb", 1000)
 ])
 def test_num_jewels_in_stones_error(jewels, stones, expected):
-    assert num_jewels_in_stones(jewels, stones) == expected
+    assert num_jewels_in_stones(jewels, stones) != expected
