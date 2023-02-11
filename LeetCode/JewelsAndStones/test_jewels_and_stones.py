cdbf29e5c4d11985925958021efdd12f46527666
Luiz Rioja <lulyrioja@gmail.com>
2023-02-11 18:40:16 -0300

    chore(jewelsAndStones): add unit tests

---
+++ LeetCode/JewelsAndStones/test_jewels_and_stones.py
@@ -0,0 +1,18 @@
+import pytest
+from jewels_and_stones import num_jewels_in_stones
+
+
+@pytest.mark.parametrize("jewels,stones,expected", [
+    ("z", "ZZ", 0),
+    ("aA", "aAAbbbb", 3)
+])
+def test_num_jewels_in_stones(jewels, stones, expected):
+    assert num_jewels_in_stones(jewels, stones) == expected
+
+
+@pytest.mark.parametrize("jewels,stones,expected", [
+    ("z", "ZZ", 0),
+    ("aA", "aAAbbbb", 3)
+])
+def test_num_jewels_in_stones_error(jewels, stones, expected):
+    assert num_jewels_in_stones(jewels, stones) == expected
