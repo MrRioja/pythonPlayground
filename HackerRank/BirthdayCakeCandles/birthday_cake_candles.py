a3188ec416fa8931b243a0da4d6e89f8e1bf87ab
Luiz Rioja <lulyrioja@gmail.com>
2023-02-11 19:09:02 -0300

    test(birthdayCakeCandles): add unit tests

--- HackerRank/BirthdayCakeCandles/birthday_cake_candles.py
+++ HackerRank/BirthdayCakeCandles/birthday_cake_candles.py
@@ -4,8 +4,6 @@
 # They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
 # The maximum height candles are 4 units high.
 
-candles = [4, 5, 1, 9, 10, 900, 30, 4, 4, 7, 4]
-
 
 def birth_day_cake_candles(candles):
     tallest = max(int(candle) for candle in candles)
@@ -16,6 +14,3 @@ def birth_day_cake_candles(candles):
             total += 1
 
     return total
-
-
-birth_day_cake_candles(candles)
---
+++ HackerRank/BirthdayCakeCandles/test_birthday_cake_candles.py
@@ -0,0 +1,18 @@
+import pytest
+from birthday_cake_candles import birth_day_cake_candles
+
+
+@pytest.mark.parametrize("candles,expected", [
+    ([4, 5, 1, 9, 10, 900, 30, 4, 4, 7, 4], 1),
+    ([3, 2, 1, 3], 2),
+])
+def test_birth_day_cake_candles(candles, expected):
+    assert birth_day_cake_candles(candles) == expected
+
+
+@pytest.mark.parametrize("candles,expected", [
+    ([4, 5, 1, 9, 10, 900, 30, 4, 4, 7, 4], 0),
+    ([3, 2, 1, 3], 0),
+])
+def test_birth_day_cake_candles_error(candles, expected):
+    assert birth_day_cake_candles(candles) != expected
