9667cc2599252fff21f5e00885d4c90320179b7e
Luiz Rioja <lulyrioja@gmail.com>
2023-02-11 20:36:09 -0300

    test(lonelyInteger): add unit tests

---
+++ HackerRank/LonelyInteger/test_lonely_integer.py
@@ -0,0 +1,16 @@
+import pytest
+from lonely_integer import lonely_integer
+
+
+@pytest.mark.parametrize("array,expected", [
+    ([1, 2, 3, 4, 3, 2, 1], 4)
+])
+def test_lonely_integer(array, expected):
+    assert lonely_integer(array) == expected
+
+
+@pytest.mark.parametrize("array,expected", [
+    ([1, 2, 3, 4, 3, 2, 1], 0)
+])
+def test_lonely_integer_error(array, expected):
+    assert lonely_integer(array) != expected
