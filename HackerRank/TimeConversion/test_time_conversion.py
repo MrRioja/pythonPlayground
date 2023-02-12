1119467ffbd1e554661e3a922e9e8a4f20306d12
Luiz Rioja <lulyrioja@gmail.com>
2023-02-11 21:28:41 -0300

    test(timeConversion): add one more test case

--- HackerRank/TimeConversion/test_time_conversion.py
+++ HackerRank/TimeConversion/test_time_conversion.py
@@ -7,6 +7,7 @@ from time_conversion import time_conversion
     ("12:37:58AM", "00:37:58"),
     ("08:37:58PM", "20:37:58"),
     ("07:45:18AM", "07:45:18"),
+    ("12:45:18PM", "12:45:18"),
 ])
 def test_time_conversion(time, expected):
     assert time_conversion(time) == expected
