4cc57c686319ef9d603498b3dd18d961685d4cee
Luiz Rioja <lulyrioja@gmail.com>
2023-02-18 21:43:44 -0300

    chore: add new challenge to solve

---
+++ HackerRank/Pangrams/pangrams.py
@@ -0,0 +1,8 @@
+# problem: https://www.hackerrank.com/challenges/pangrams/problem
+# A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram
+# in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
+# Example: s = "The quick brown fox jumps over the lazy dog"
+# The string contains all letters in the English alphabet, so return pangram.
+
+def pangrams(s):
+    return
