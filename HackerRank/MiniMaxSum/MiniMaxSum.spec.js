7566a23d48f8566b3ef21e5009d85d4505a67b8a
Luiz Rioja <lulyrioja@gmail.com>
2023-02-20 15:50:27 -0300

    test(minMaxSum): add test description

--- HackerRank/MiniMaxSum/MiniMaxSum.spec.js
+++ HackerRank/MiniMaxSum/MiniMaxSum.spec.js
@@ -6,7 +6,10 @@ describe("MiniMaxSum", () => {
   it.each([
     { arr: [1, 3, 5, 7, 9], expected: [16, 24] },
     { arr: [7, 69, 2, 221, 8974], expected: [299, 9271] },
-  ])("should be able ", ({ arr, expected }) => {
-    expect(miniMaxSum(arr)).toEqual(expected);
-  });
+  ])(
+    "should be able to find the minimum and maximum sum values in an array",
+    ({ arr, expected }) => {
+      expect(miniMaxSum(arr)).toEqual(expected);
+    }
+  );
 });
