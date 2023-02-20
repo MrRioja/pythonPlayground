368296e5c5e4dea8a7ff6b11bcabea461a5e7977
Luiz Rioja <lulyrioja@gmail.com>
2023-02-20 16:32:03 -0300

    test: improve tests cov

--- HackerRank/NumberLineJumps/numberLineJumps.spec.js
+++ HackerRank/NumberLineJumps/numberLineJumps.spec.js
@@ -5,6 +5,7 @@ import { kangaroo } from "./numberLineJumps";
 describe("Number line jumps", () => {
   it.each([
     { x1: 0, v1: 2, x2: 5, v2: 3, expected: "NO" },
+    { x1: 21, v1: 6, x2: 47, v2: 3, expected: "NO" },
     { x1: 0, v1: 3, x2: 4, v2: 2, expected: "YES" },
     { x1: 4523, v1: 8092, x2: 9419, v2: 8076, expected: "YES" },
   ])(
