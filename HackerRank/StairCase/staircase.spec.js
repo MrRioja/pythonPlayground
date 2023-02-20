d4e6afb2f0c3ca8b71101cb85a1f2681d6d43e0e
Luiz Rioja <lulyrioja@gmail.com>
2023-02-20 16:06:56 -0300

    test(staircase): add unit test

---
+++ HackerRank/StairCase/staircase.spec.js
@@ -0,0 +1,12 @@
+import { describe, expect, it, assert } from "vitest";
+
+import { staircase } from "./staircase";
+
+describe("Stair case", () => {
+  it.each([
+    { num: 3, expected: "  #\n ##\n###" },
+    { num: 4, expected: "   #\n  ##\n ###\n####" },
+  ])("should be able to build a ladder of size N", ({ num, expected }) => {
+    expect(staircase(num)).toEqual(expected);
+  });
+});
