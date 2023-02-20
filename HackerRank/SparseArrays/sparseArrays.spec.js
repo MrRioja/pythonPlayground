b5b7e1bfa2882ec179893bbb5664b90f9a5cc8f7
Luiz Rioja <lulyrioja@gmail.com>
2023-02-20 15:48:52 -0300

    test(sparseArrays): add unit test

---
+++ HackerRank/SparseArrays/sparseArrays.spec.js
@@ -0,0 +1,34 @@
+import { describe, expect, it } from "vitest";
+
+import { matchingStrings } from "./sparseArrays";
+
+describe("SparseArrays", () => {
+  it.each([
+    {
+      queries: ["abcde", "sdaklfj", "asdjf", "na", "basdn"],
+      strings: [
+        "13",
+        "abcde",
+        "sdaklfj",
+        "asdjf",
+        "na",
+        "basdn",
+        "sdaklfj",
+        "asdjf",
+        "na",
+        "asdjf",
+        "na",
+        "basdn",
+        "sdaklfj",
+        "asdjf",
+        "5",
+      ],
+      expected: [1, 3, 4, 3, 2],
+    },
+  ])(
+    "should be able to determine how many times each query string occurs in the list of input strings",
+    ({ strings, queries, expected }) => {
+      expect(matchingStrings(strings, queries)).toEqual(expected);
+    }
+  );
+});
