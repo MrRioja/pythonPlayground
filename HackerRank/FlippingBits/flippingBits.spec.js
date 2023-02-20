ee3c7f634273d5073a33b89ecc29f7bbb9ab0a35
Luiz Rioja <lulyrioja@gmail.com>
2023-02-20 14:47:57 -0300

    test(flippingBits): change expected value in a test case

--- HackerRank/FlippingBits/flippingBits.spec.js
+++ HackerRank/FlippingBits/flippingBits.spec.js
@@ -7,7 +7,7 @@ describe("Flipping bits", () => {
     { num: 0, expected: 4294967295 },
     { num: 1, expected: 4294967294 },
     { num: 9, expected: 4294967286 },
-    { num: 2147483647, expected: 1 },
+    { num: 2147483647, expected: 2147483648 },
   ])("should be able to flip bits", ({ num, expected }) => {
     expect(flippingBits(num)).toEqual(expected);
   });
