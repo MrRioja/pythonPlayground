7c9bcd02d3aac905e9b5af37a2560c9ceada8c2a
Luiz Rioja <lulyrioja@gmail.com>
2023-02-20 15:44:07 -0300

    test(simpleArraySum): add unit test

--- HackerRank/SimpleArraySum/simpleArraySum.js
+++ HackerRank/SimpleArraySum/simpleArraySum.js
@@ -2,14 +2,10 @@
 // Given an array of integers, find the sum of its elements.
 // For example, if the array ar=[1,2,3], so return 6.
 
-const vector = [338, 65, 713, 595, 428, 610, 728, 573, 871, 868];
-
-function sumArrayElements(vector) {
+export function sumArrayElements(vector) {
   let total = 0;
 
   vector.forEach((el) => (total = total + el));
 
   return total;
 }
-
-sumArrayElements(vector);
