f71896c1daf942fbca56bc7ff6cbf177f3a3e523
Luiz Rioja <lulyrioja@gmail.com>
2023-02-24 18:16:21 -0300

    feat(camelcase4): solve challenge with Javascript

--- HackerRank/CamelCase4/camelCase4.js
+++ HackerRank/CamelCase4/camelCase4.js
@@ -3,3 +3,31 @@
 // start with a lowercase letter, with all subsequent words starting with a capital letter (example: startThread).
 // Names of classes follow the same pattern, except that they start with a capital letter (example: BlueCar).
 // Your task is to write a program that creates or splits Camel Case variable, method, and class names.
+
+export function camelCase(word) {
+  let [operation, typeValue, value] = word.split(";");
+
+  if (operation == "S") {
+    const allMatch = value.match(/[A-Z]/g);
+
+    allMatch.forEach((match) => {
+      value = value.replace(match, ` ${match.toLowerCase()}`);
+    });
+
+    return value.replace("()", "").trim();
+  } else {
+    const allMatch = value.match(/\s\w/g);
+
+    allMatch.forEach((match) => {
+      value = value.replace(match, match.trim().toUpperCase());
+    });
+
+    if (typeValue == "V") {
+      return value;
+    } else if (typeValue == "M") {
+      return value + "()";
+    } else {
+      return value.replace(value[0], value[0].toUpperCase());
+    }
+  }
+}
