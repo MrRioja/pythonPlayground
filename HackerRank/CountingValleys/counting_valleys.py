9b155f99d6b5d1a71a26ed5fd5dfd4f5846b7f87
Luiz Rioja <lulyrioja@gmail.com>
2023-02-26 21:36:33 -0300

    chore(countingvalleys): add problem link

--- HackerRank/CountingValleys/counting_valleys.py
+++ HackerRank/CountingValleys/counting_valleys.py
@@ -1,3 +1,4 @@
+# Problem: https://www.hackerrank.com/challenges/counting-valleys/problem
 # An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly steps steps,
 # for every step it was noted if it was an uphill, U, or a downhill, D step. Hikes always start and end at sea level,
 # and each step up or down represents a 1 unit change in altitude. We define the following terms:
