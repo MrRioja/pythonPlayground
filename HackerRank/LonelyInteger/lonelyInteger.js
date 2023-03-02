576e271a42a36605c62ead75dda5ff7b71b5f9ee
Luiz Rioja <lulyrioja@gmail.com>
2023-03-02 14:02:23 -0300

    refactor(lonelyinteger): remove index from filter

--- HackerRank/LonelyInteger/lonelyInteger.js
+++ HackerRank/LonelyInteger/lonelyInteger.js
@@ -3,7 +3,7 @@
 // Ex: a = [1, 2, 3, 4, 3, 2, 1]. The unique element is 4.
 
 export function lonelyInteger(a) {
-  const lonelyIntegers = a.filter((element, index) => {
+  const lonelyIntegers = a.filter((element) => {
     if (
       a.findIndex((num) => num === element) ===
       a.findLastIndex((num) => num === element)
