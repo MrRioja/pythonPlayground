2cf7580835927267b544bac1456f59b361314fc8
Luiz Rioja <lulyrioja@gmail.com>
2023-04-12 15:00:00 -0300

    chore: fix problem description

--- HackerRank/MigratoryBirds/MigratoryBirds.js
+++ HackerRank/MigratoryBirds/MigratoryBirds.js
@@ -1,7 +1,7 @@
 // Problem: https://www.hackerrank.com/challenges/migratory-birds/problem
 // Given an array of bird sightings where every element represents a bird type id, determine the id of the most
 // frequently sighted type. If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
-// Example: There are two each of types  and , and one sighting of type . Pick the lower of the two types seen twice: type .
+// Example: There are two each of types 1 and 2, and one sighting of type 3. Pick the lower of the two types seen twice: type 1.
 
 function migratoryBirds(arr) {
   return
