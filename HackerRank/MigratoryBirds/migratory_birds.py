8eeeddb0afefecb1a556e1cf966fd11beddf071c
Luiz Rioja <lulyrioja@gmail.com>
2023-04-14 15:00:26 -0300

    feat(migratoryBirds): solve challenge with Python

--- HackerRank/MigratoryBirds/migratory_birds.py
+++ HackerRank/MigratoryBirds/migratory_birds.py
@@ -3,5 +3,13 @@
 # frequently sighted type. If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
 # Example: There are two each of types 1 and 2, and one sighting of type 3. Pick the lower of the two types seen twice: type 1.
 
+from collections import Counter
+
+
 def migratory_birds(arr):
-    return
+    total_per_items = Counter(arr)
+    [(_, max_frequency)] = total_per_items.most_common(1)
+    items_with_max_frequency = sorted([
+        num for num, total in total_per_items.most_common() if total == max_frequency])
+
+    return items_with_max_frequency[0]
