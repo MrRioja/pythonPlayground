dfc324df7ab4b3c5270a78b59cf9de010e218fb9
Luiz Rioja <lulyrioja@gmail.com>
2023-04-15 16:04:10 -0300

    refactor(sparseArrays): remove commented solutions

--- HackerRank/SparseArrays/sparse_arrays.py
+++ HackerRank/SparseArrays/sparse_arrays.py
@@ -5,38 +5,10 @@
 # queries = ['ab', 'abc', 'bc']
 # There are 2 instances of 'ab', 1 of 'abc' and 0 of 'bc'. For each query, add an element to the return array, results = [2, 1, 0].
 
-# Other solution:
-# from collections import Counter
-
-
-# def matching_strings(strings, queries):
-
-#     strings_count = Counter(strings)
-#     return [strings_count[x] for x in queries]
-
-# def matching_strings(strings, queries):
-#     return [
-#         strings.count(query)
-#         for query in queries
-#     ]
+from collections import Counter
 
 
 def matching_strings(strings, queries):
-    total_per_item = {
-        query: 0
-        for query in queries
-    }
-    results = []
 
-    for query in queries:
-        if total_per_item[query] > 0:
-            results.append(total_per_item[query])
-
-            continue
-
-        for string in strings:
-            if query == string:
-                total_per_item[query] += 1
-
-        results.append(total_per_item[query])
-    return results
+    strings_count = Counter(strings)
+    return [strings_count[x] for x in queries]
