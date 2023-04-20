13b064b3ee976473c4cc6158c71c46245839187c
Luiz Rioja <lulyrioja@gmail.com>
2023-04-20 20:55:31 -0300

    feat(xorStrings3): solve challenge with python

---
+++ HackerRank/XORStrings3/xor_strings_3.py
@@ -0,0 +1,17 @@
+# Problem: https://www.hackerrank.com/challenges/three-month-preparation-kit-strings-xor/problem
+# In this challenge, the task is to debug the existing code to successfully execute all provided test files.
+# Given two strings consisting of digits 0 and 1 only, find the XOR of the two strings.
+# Debug the given function strings_xor to find the XOR of the two given strings appropriately.
+# Note: You can modify at most three lines in the given code and you cannot add or remove lines to the code.
+# To restore the original code, click on the icon to the right of the language selector.
+
+def strings_xor(s, t):
+    res = ""
+
+    for i in range(len(s)):
+        if s[i] == t[i]:
+            res += '0'
+        else:
+            res += '1'
+
+    return res
