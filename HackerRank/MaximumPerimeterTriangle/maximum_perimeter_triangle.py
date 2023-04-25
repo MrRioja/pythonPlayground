2b60296bcee46be8c094451ecefef9e54c002cae
Luiz Rioja <lulyrioja@gmail.com>
2023-04-25 17:37:35 -0300

    feat(maximumPerimeterTriangle): solve challenge with Python

--- HackerRank/MaximumPerimeterTriangle/maximum_perimeter_triangle.py
+++ HackerRank/MaximumPerimeterTriangle/maximum_perimeter_triangle.py
@@ -8,8 +8,28 @@
 # If no non-degenerate triangle exists, return [-1].
 # Example: sticks = [1, 2, 3, 4, 5, 10]
 # The triplet(1, 2, 3) will not form a triangle. Neither will(4, 5, 10) or (2, 3, 5), so the problem is reduced to
-# (2, 3, 4) and (3, 4, 5). The longer perimeter is 3 + 5 + 5 = 12.
+# (2, 3, 4) and (3, 4, 5). The longer perimeter is 3 + 4 + 5 = 12.
+
+
+from itertools import combinations
 
 
 def maximum_perimeter_triangle(sticks):
-    return
+
+    max_perimeter = 0
+    possibles_triangles = []
+    sorted_sticks = sorted(sticks)
+
+    for a, b, c in combinations(sorted_sticks, 3):
+        if a != 0 and b != 0 and c != 0 and ((a + b) > c):
+            if (a + b + c) > max_perimeter:
+                max_perimeter = a + b + c
+                possibles_triangles.clear()
+                possibles_triangles.append([a, b, c])
+            elif (a + b + c) == max_perimeter:
+                possibles_triangles.append([a, b, c])
+
+    if len(possibles_triangles) > 0:
+        return possibles_triangles[0]
+    else:
+        return [-1]
