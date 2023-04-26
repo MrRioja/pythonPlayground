951a7f1320c3eed33b89f8d1f6f7b4d463c3c673
Luiz Rioja <lulyrioja@gmail.com>
2023-04-26 10:33:13 -0300

    refactor(xorStrings3): change if else to xor operador

--- HackerRank/XORStrings3/xor_strings_3.py
+++ HackerRank/XORStrings3/xor_strings_3.py
@@ -9,9 +9,6 @@ def strings_xor(s, t):
     res = ""
 
     for i in range(len(s)):
-        if s[i] == t[i]:
-            res += '0'
-        else:
-            res += '1'
+        res += str(int(s[i]) ^ int(t[i]))
 
     return res
